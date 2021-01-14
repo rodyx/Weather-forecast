988ee8355e3a36b4277ac9cb509ca57b03fd387b
rodyx <erik.valeev.ev@gmail.com>
2021-01-15 01:48:36 +0500

    animated bottom tiles

--- src/bottomBar/BottomBar.jsx
+++ src/bottomBar/BottomBar.jsx
@@ -1,58 +1,133 @@
 import React, { useState, useEffect } from 'react';
-import { Dimensions, Platform, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, Platform, StatusBar, StyleSheet, Text, TouchableOpacity, View, Animated, Button, TouchableNativeFeedback, TouchableHighlight } from 'react-native';
 import { Ionicons, Fontisto, MaterialCommunityIcons, SimpleLineIcons, Feather } from '@expo/vector-icons';
 import { useFonts, HammersmithOne_400Regular } from '@expo-google-fonts/hammersmith-one';
 import Piles from './Piles';
 import Popup from './Popup';
 
+
+const windWidth = Dimensions.get('window').width;
+const windHeight = Dimensions.get('window').height;
+let k = [0, -300];
+
 export const BottomBar = () => {
-  let [fontsLoaded] = useFonts({
-    HammersmithOne_400Regular,
-  });
+  const box1AnimationValue = React.useRef(new Animated.Value(0)).current;
+  const box2AnimationValue = React.useRef(new Animated.Value(0)).current;
+  const box3AnimationValue = React.useRef(new Animated.Value(0)).current;
+
+  const buttonPressed = () => {
+    Animated.stagger(150, [
+      Animated.timing(box1AnimationValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.timing(box2AnimationValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.timing(box3AnimationValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+
+    ]).start()
+  }
+
+  let k = windHeight * 0.5;
+  const test = () => {
+    box1AnimationValue.setValue(0);
+    box2AnimationValue.setValue(0);
+    box3AnimationValue.setValue(0);
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.dayLine}>
-        <Text style={styles.dayLinetext}>Morning</Text>
-        <Text style={styles.dayLinetext}>Afternoon</Text>
-        <Text style={styles.dayLinetext}>Evening</Text>
+        <TouchableOpacity onPress={buttonPressed}>
+          <Text style={styles.dayLinetext}>Morning</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={buttonPressed}>
+          <Text style={styles.dayLinetext}>Afternoon</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={test}>
+          <Text style={styles.dayLinetext}>Evening</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.tileWrapper}>
         
-        <TouchableOpacity onPress={() => alert('edikl')}>
-          <View style={styles.tile}>
-            <Text style={styles.tileTime}>7:00</Text>
-            <Text><Feather name="sun" size={55} color="white" /></Text>
-            <Text style={styles.tileTemp}>24°</Text>
-          </View>
-        </TouchableOpacity>
+        
+        <Animated.View style={{
+          transform: [
+            {
+              translateY: box1AnimationValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, -700],
+              }),
+            }
+          ],
+        }}>
+          <TouchableOpacity onPress={() => alert('edikl')}>
+            <View style={styles.tile}>
+              <Text style={styles.tileTime}>19:00</Text>
+              <Text><Feather name="sun" size={55} color="white" /></Text>
+              <Text style={styles.tileTemp}>21°</Text>
+            </View>
+          </TouchableOpacity>
+        </Animated.View>
 
-        <TouchableOpacity onPress={() => alert('edikl')}>
-          <View style={styles.tile} onPress={() => alert('edikl')}>
 
-            <Text style={styles.tileTime}>13:00</Text>
-            <Text><Ionicons name="ios-rainy-outline" size={55} color="white" /></Text>
-            <Text style={styles.tileTemp}>18°</Text>
-          </View>
-        </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => alert('edikl')}>
-        <View style={styles.tile}>
-          <Text style={styles.tileTime}>19:00</Text>
-          <Text><Ionicons name="ios-cloudy-outline" size={55} color="white" /></Text>
-          <Text style={styles.tileTemp}>21°</Text>
-        </View>
-        </TouchableOpacity>
+        <Animated.View style={{
+          transform: [
+            {
+              translateY: box2AnimationValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, -700],
+              }),
+            }
+          ],
+        }}>
+          <TouchableOpacity onPress={() => alert('222edikl')}>
+            <View style={styles.tile}>
+              <Text style={styles.tileTime}>13:00</Text>
+              <Text><Ionicons name="ios-rainy-outline" size={55} color="white" /></Text>
+              <Text style={styles.tileTemp}>18°</Text>
+            </View>
+          </TouchableOpacity>
+        </Animated.View>
+
+
+
+        <Animated.View style={{
+          transform: [
+            {
+              translateY: box3AnimationValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, -700],
+              }),
+            }
+          ],
+        }}>
+          <TouchableOpacity onPress={() => alert('edikl')}>
+            <View style={styles.tile}>
+              <Text style={styles.tileTime}>19:00</Text>
+              <Text><Ionicons name="ios-cloudy-outline" size={55} color="white" /></Text>
+              <Text style={styles.tileTemp}>21°</Text>
+            </View>
+          </TouchableOpacity>
+        </Animated.View>
+
+
       </View>
-
       <StatusBar style="gray" />
     </View>
      
   )
 }
 
-const windWidth = Dimensions.get('window').width;
-const windHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
@@ -74,11 +149,14 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    height: windHeight,
+    
   },
   tile: {
     width: windWidth / 3.7,
     height: windHeight / 5,
-    backgroundColor: '#87d8fc',
+    backgroundColor: '#88d9fc',
     borderRadius: 15,
     justifyContent: 'space-evenly',
     alignItems: 'center',
@@ -91,5 +169,5 @@ const styles = StyleSheet.create({
     fontSize: 40,
     fontFamily: 'HammersmithOne_400Regular',
     color: "#41406e",
-  }
+  },
 });
